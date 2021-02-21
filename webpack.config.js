035a23dfe05023ce125070b03a0d99a49be8ddbc
isavlex <mailsavlex@gmail.com>
2021-02-21 10:08:26 +0300

    changed implementation of skills

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const fs = require('fs');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const fs = require('fs')
 const CopyPlugin = require('copy-webpack-plugin')
 const isDev = process.env.NODE_ENV === 'development'
-const filename = (ext) => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
-
+const isProd = !isDev
+const filename = (ext) => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`)
 
 const PATHS = {
   src: path.join(__dirname, './src'),
@@ -29,8 +32,34 @@ const jsLoaders = () => {
   return loaders
 }
 
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendors',
+          test: /node_modules/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  }
+
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ]
+  }
+
+  return config
+}
+
 const PAGES_DIR = `${PATHS.src}/pug/pages/`
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .filter((fileName) => fileName.endsWith('.pug'))
 
 module.exports = {
   mode: 'development',
@@ -39,18 +68,7 @@ module.exports = {
     filename: filename('js'),
     path: PATHS.dist,
   },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          name: 'vendors',
-          test: /node_modules/,
-          chunks: 'all',
-          enforce: true
-        }
-      }
-    }
-  },
+  optimization: optimization(),
   resolve: {
     extensions: ['.js'],
     alias: {
@@ -62,47 +80,47 @@ module.exports = {
   devServer: {
     contentBase: './dist',
   },
-  
+
   plugins: [
     new CleanWebpackPlugin(),
-    ...PAGES.map(page => new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/${page}`,
-      filename: `./${page.replace(/\.pug/,'.html')}`
-    })),
+    ...PAGES.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          template: `${PAGES_DIR}/${page}`,
+          filename: `./${page.replace(/\.pug/, '.html')}`,
+        })
+    ),
     new CopyPlugin({
       patterns: [
         { from: `${PATHS.src}/static/img`, to: `${PATHS.assets}img` },
         { from: `${PATHS.src}/icons/`, to: `${PATHS.assets}img/icons` },
       ],
     }),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
   ],
-  
+
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader',
-        ],
+        use: ['file-loader'],
       },
       {
         test: /\.pug$/,
-        loader: 'pug-loader'
+        loader: 'pug-loader',
       },
       {
         test: /\.(png|svg|jpg|gif|webp)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]'
-        }
+          name: '[name].[ext]',
+        },
       },
       {
         test: /\.js$/,
@@ -111,4 +129,4 @@ module.exports = {
       },
     ],
   },
-};
+}
